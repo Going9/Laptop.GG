e5cfd501dfb26f96d0acbf6615f7ad4ada5275ae
Going9 <ickdor123@gmail.com>
2024-04-05 13:25:08 +0900

    Init: start kotlin spring boot project

---
+++ LaptopGG/src/main/kotlin/Going9/LaptopGG/LaptopGgApplication.kt
@@ -0,0 +1,11 @@
+package Going9.LaptopGG
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaptopGgApplication
+
+fun main(args: Array<String>) {
+	runApplication<LaptopGgApplication>(*args)
+}
---
+++ LaptopGG/src/test/kotlin/Going9/LaptopGG/LaptopGgApplicationTests.kt
@@ -0,0 +1,13 @@
+package Going9.LaptopGG
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaptopGgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
