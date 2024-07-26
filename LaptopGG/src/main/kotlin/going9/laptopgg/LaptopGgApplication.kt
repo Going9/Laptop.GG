29586cceb8cc76fc7b92d2560e8846ab8e38fb43
Going9 <ickdor123@gmail.com>
2024-07-27 02:02:26 +0900

    f

--- LaptopGG/src/main/kotlin/going9/laptopgg/LaptopGgApplication.kt
+++ LaptopGG/src/main/kotlin/going9/laptopgg/LaptopGgApplication.kt
@@ -1,12 +1,24 @@
 package going9.laptopgg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import javax.sql.DataSource
 
 @SpringBootApplication
 @EnableJpaRepositories
-class LaptopGgApplication
+class LaptopGgApplication {
+
+	@Bean
+	fun init(dataSource: DataSource): CommandLineRunner {
+		return CommandLineRunner {
+			println("Database URL: ${dataSource.connection.metaData.url}")
+			println("Database Username: ${dataSource.connection.metaData.userName}")
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<LaptopGgApplication>(*args)
