ad195914e7fe81d1e306cbc41b265edb53c05a4f
Going9 <ickdor123@gmail.com>
2024-08-08 14:44:09 +0900

    Add: add thymeleaf

--- backend/LaptopGG/build.gradle.kts
+++ backend/LaptopGG/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.xerial:sqlite-jdbc:3.41.2.2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
