5b57e0969185a4fc34f17a6686684922baa3433b
Going9 <ickdor123@gmail.com>
2024-09-09 19:00:16 +0900

    Fix: delete spring boot security default page

--- LaptopGG/src/main/kotlin/going9/laptopgg/LaptopGgApplication.kt
+++ LaptopGG/src/main/kotlin/going9/laptopgg/LaptopGgApplication.kt
@@ -2,12 +2,13 @@ package going9.laptopgg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import javax.sql.DataSource
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @EnableJpaRepositories
 class LaptopGgApplication {
 
