090c285118ff6887de511a00c99c2cb8b8805067
Going9 <ickdor123@gmail.com>
2024-11-06 22:35:12 +0900

    Fixs: add chrome options

--- LaptopGG/src/main/kotlin/going9/laptopgg/configs/WebDriverConfig.kt
+++ LaptopGG/src/main/kotlin/going9/laptopgg/configs/WebDriverConfig.kt
@@ -12,7 +12,12 @@ class WebDriverConfig {
     @Bean
     fun webDriver(): WebDriver {
         val options = ChromeOptions()
-        options.addArguments("--headless")
+        options.addArguments("--headless") // GUI 없이 실행
+        options.addArguments("--no-sandbox") // 권한 문제 회피
+        options.addArguments("--disable-dev-shm-usage") // /dev/shm 사용하지 않음
+        options.addArguments("--disable-gpu") // GPU 비활성화 (리눅스 환경에서는 불필요)
+        options.addArguments("--remote-allow-origins=*") // CORS 문제 회피
+
         return ChromeDriver(options)
     }
 }
