2a9f049fbe95082c9150d68be4e422baaa28bda3
Going9 <ickdor123@gmail.com>
2024-11-13 14:40:13 +0900

    Test: virtual threading

--- LaptopGG/src/test/kotlin/going9/laptopgg/service/cralwer/CrawlerServiceTest.kt
+++ LaptopGG/src/test/kotlin/going9/laptopgg/service/cralwer/CrawlerServiceTest.kt
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.Executors
 
 @SpringBootTest(properties = ["spring.profiles.active=test"])
 class CrawlerServiceTest(
@@ -25,42 +26,81 @@ class CrawlerServiceTest(
 
     @Test
     fun `test fetchProductList retrieves product list`() {
-        // 초기 작업 수행
-        crawlerService.loadLaptopPage()
-        crawlerService.setupFilters()
-        Thread.sleep(5000)
+        var executor = Executors.newVirtualThreadPerTaskExecutor()
 
-        // 크롤링 루프 시작
-        while (true) {
-            // 제품 리스트 가져오기
-            val products = crawlerService.fetchProductList()
+        try {
+            // 초기 작업 수행
+            crawlerService.loadLaptopPage()
+            crawlerService.setupFilters()
+            Thread.sleep(5000)
 
-            // 제품 정보 파싱 및 엔티티 생성
-            val laptopsToSave = mutableListOf<NewLaptop>()
-            for (product in products) {
-                val newLaptop = crawlerService.parseProductDetails(product)
-                newLaptop?.let { laptop ->
-                    val existingLaptop = newLaptopRepository.findByName(laptop.name)
-                    if (existingLaptop != null) {
-                        // 이미 존재하는 경우 가격만 업데이트
-                        existingLaptop.price = laptop.price
-                        newLaptopRepository.save(existingLaptop)
-                    } else {
-                        laptopsToSave.add(laptop)
+            // 크롤링 루프 시작
+            while (true) {
+                // 제품 리스트 가져오기
+                val products = crawlerService.fetchProductList()
+
+                // 제품 정보 파싱 및 엔티티 생성
+                val futures = products.map { product ->
+                    executor.submit<NewLaptop?> {
+                        val newLaptop = crawlerService.parseProductDetails(product)
+                        if (newLaptop != null) {
+                            val existingLaptop = newLaptopRepository.findByName(newLaptop.name)
+                            if (existingLaptop != null) {
+                                existingLaptop.price = newLaptop.price
+                                newLaptopRepository.save(existingLaptop)
+                                println(executor.toString())
+                                null
+                            } else {
+                                newLaptop
+                            }
+                        } else {
+                            null
+                        }
                     }
                 }
-            }
 
-            // 데이터베이스에 새로운 랩탑 정보 저장
-            if (laptopsToSave.isNotEmpty()) {
-                println("새로운 노트북 정보를 저장합니다.")
-                newLaptopRepository.saveAll(laptopsToSave)
-            }
+                val laptopsToSave = futures.mapNotNull { it.get() }
 
-            // 다음 페이지로 이동, 마지막 페이지 도달 시 종료
-            if (!crawlerService.navigateToNextPage()) break
+                if (laptopsToSave.isNotEmpty()) {
+                    println("새로운 노트북 정보를 저장합니다.")
+                    newLaptopRepository.saveAll(laptopsToSave)
+                }
+
+                // 다음 페이지로 이동, 마지막 페이지 도달 시 종료
+                if (!crawlerService.navigateToNextPage()) break
+            }
+        } finally {
+            executor.shutdown()
         }
     }
+
+
+
+//            val laptopsToSave = mutableListOf<NewLaptop>()
+//            for (product in products) {
+//                val newLaptop = crawlerService.parseProductDetails(product)
+//                newLaptop?.let { laptop ->
+//                    val existingLaptop = newLaptopRepository.findByName(laptop.name)
+//                    if (existingLaptop != null) {
+//                        // 이미 존재하는 경우 가격만 업데이트
+//                        existingLaptop.price = laptop.price
+//                        newLaptopRepository.save(existingLaptop)
+//                    } else {
+//                        laptopsToSave.add(laptop)
+//                    }
+//                }
+//            }
+
+//            // 데이터베이스에 새로운 랩탑 정보 저장
+//            if (laptopsToSave.isNotEmpty()) {
+//                println("새로운 노트북 정보를 저장합니다.")
+//                newLaptopRepository.saveAll(laptopsToSave)
+//            }
+//
+//            // 다음 페이지로 이동, 마지막 페이지 도달 시 종료
+////            if (!crawlerService.navigateToNextPage()) break
+//        }
+//    }
 
     @AfterEach
     fun tearDown() {
