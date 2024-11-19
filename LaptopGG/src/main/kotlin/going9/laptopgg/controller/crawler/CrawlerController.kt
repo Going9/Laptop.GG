1faf3084687e21e5466afd9fc5c8d568a781fc66
Going9 <ickdor123@gmail.com>
2024-11-19 16:42:09 +0900

    Fix: compose settings

--- LaptopGG/src/main/kotlin/going9/laptopgg/controller/crawler/CrawlerController.kt
+++ LaptopGG/src/main/kotlin/going9/laptopgg/controller/crawler/CrawlerController.kt
@@ -1,9 +1,8 @@
 package going9.laptopgg.controller.crawler
 
+import going9.laptopgg.service.crawler.CrawlerService
 import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,21 +12,56 @@ import org.springframework.web.bind.annotation.RestController
 class CrawlerController(
     private val jobLauncher: JobLauncher,
     private val laptopJob: Job,
+    private val crawlerService: CrawlerService,
 ) {
 
+//    @GetMapping("/laptops")
+//    fun startCrawling(): ResponseEntity<String> {
+//        return try {
+//            // 크롤링 잡 실행
+//            val jobParameters = JobParametersBuilder()
+//                .addLong("time", System.currentTimeMillis())
+//                .toJobParameters()
+//
+//            jobLauncher.run(laptopJob, jobParameters)
+//
+//            ResponseEntity.ok("크롤링 시작")
+//        } catch (e: Exception) {
+//            ResponseEntity.status(500).body("${e}")
+//        }
+//    }
+
     @GetMapping("/laptops")
-    fun startCrawling(): ResponseEntity<String> {
-        return try {
-            // 크롤링 잡 실행
-            val jobParameters = JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters()
+    fun startCrawling() {
+        // 초기 작업 수행
+        crawlerService.loadLaptopPage()
+        crawlerService.setupFilters()
+        Thread.sleep(5000)
 
-            jobLauncher.run(laptopJob, jobParameters)
+        // 크롤링 루프 시작
+        while (true) {
+            val startTime = System.currentTimeMillis()
 
-            ResponseEntity.ok("크롤링 시작")
-        } catch (e: Exception) {
-            ResponseEntity.status(500).body("${e}")
+            val products = crawlerService.fetchProductList()
+
+            // 스레드 안정성을 위해서 메인 스레드에서 제품 데이터 추출
+            val productDataList = products.map { product ->
+                crawlerService.extractProductData(product)
+            }
+
+            productDataList.map { productData ->
+                val newLaptop = crawlerService.parseProductDetails(productData)
+                if (newLaptop != null) {
+                    crawlerService.saveOrUpdateLaptop(newLaptop)
+                }
+            }
+
+            val endTime = System.currentTimeMillis()
+
+            println("걸린시간: " + (endTime - startTime))
+
+            // 다음 페이지로 이동, 마지막 페이지 도달 시 종료
+            if (!crawlerService.navigateToNextPage()) break
         }
     }
 }
